7c3e9aae926832e1a7fe034b98db3cb7964fa4a0
Lucas Stephan <lucas.stephan@epita.fr>
2024-02-07 15:30:03 +0200

    init controller

---
+++ controller/controller.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+CMD='mosquitto_pub -p 1883 -h localhost -I controller -t '
+array=("Distance" "Move" "Turn")
+
+error() {
+	echo "$0 [Topic] [payload]"
+	echo ""
+	echo "Topic:"
+	echo "    ${array[@]}"
+	exit 1
+}
+
+if [ $# -ne 2 ]; then
+	error
+fi
+
+for i in "${array[@]}"
+do
+  if [[ $i == $1 ]]
+  then
+	  $CMD Lego/$i -m "$2" && exit 0
+  fi
+done
+
+error
